15bf697e8d116dd9a2acf8af7e0fb9cc771553dc
sasay7783 <sasay7783@nate.com>
2023-12-13 22:25:43 +0900

    [Fix] MoneyData Complete
    입금 출금 기능은 다 해결을 했었는데 값이 변경이 되지 않아서 고생했다.
    현재는 그냥 PlayerPrefs로 해결했지만 값이 고정되어있어서 나중에 어떻게 해야할지 고민을 해야할 듯 하다.

--- Assets/Scripts/atmManager.cs
+++ Assets/Scripts/atmManager.cs
@@ -8,20 +8,43 @@ using UnityEngine.UI;
 
 public class atmManager : MonoBehaviour
 {
+    public static atmManager _instance;
+
     public TextMeshProUGUI userMoney;
     public TextMeshProUGUI cardMoney;
+    public TextMeshProUGUI userName;
+    [SerializeField] private GameObject insufficient;
     [SerializeField] private GameObject _inputField;
-    int money;
-    int card;
-    int moveMoney;
+    int money = 0;
+    int card = 0;
+    int moveMoney = 0;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        money = int.Parse(userMoney.text);
-        card = int.Parse(cardMoney.text);
+        insufficient.SetActive(false);
+
+        userName.text = SceneChange.instance.userName.text;
+        money = int.Parse(SceneChange.instance.userMoney.text);
+        card = int.Parse(SceneChange.instance.cardMoney.text);
+
+        userMoney.text = money.ToString();
+        cardMoney.text = card.ToString();
     }
 
+    // 버튼 입출금
     public void deposit_withdraw()
     {
         moveMoney = 0;
@@ -33,6 +56,7 @@ public class atmManager : MonoBehaviour
             if (moveMoney > money)
             {
                 // 선택한 금액보다 현금이 적기 때문
+                insufficient.SetActive(true);
             }
             else if (money >= moveMoney)
             {
@@ -45,6 +69,7 @@ public class atmManager : MonoBehaviour
             if (moveMoney > card)
             {
                 // 선택한 금액보다 카드 금액이 적기 때문
+                insufficient.SetActive(true);
             }
             else if (moveMoney <= card)
             {
@@ -56,16 +81,25 @@ public class atmManager : MonoBehaviour
         cardMoney.text = card.ToString();
     }
 
+    // 입력 입출금
     public void Input_deposit_withdraw()
     {
         moveMoney = 0;
-        moveMoney = int.Parse(_inputField.GetComponent<TMP_InputField>().text);
+        if (_inputField != null && _inputField.GetComponent<TMP_InputField>() != null)
+        {
+            moveMoney = int.Parse(_inputField.GetComponent<TMP_InputField>().text);
+        }
+        else
+        {
+            print("실행되지 않음");
+        }
 
         if (SceneManager.GetActiveScene().name == "DepositScene")
         {
             if (moveMoney > money)
             {
                 // 선택한 금액보다 현금이 적기 때문
+                insufficient.SetActive(true);
             }
             else if (money >= moveMoney)
             {
@@ -78,6 +112,7 @@ public class atmManager : MonoBehaviour
             if (moveMoney > card)
             {
                 // 선택한 금액보다 카드 금액이 적기 때문
+                insufficient.SetActive(true);
             }
             else if (moveMoney <= card)
             {
@@ -85,7 +120,18 @@ public class atmManager : MonoBehaviour
                 card -= moveMoney;
             }
         }
-            userMoney.text = money.ToString();
-            cardMoney.text = card.ToString();
+        _inputField.GetComponent<TMP_InputField>().text = "";
+        userMoney.text = money.ToString();
+        cardMoney.text = card.ToString();
+    }
+
+    public void SceneBack()
+    {
+        SceneChange.instance.SaveData(userName.text, money, card);
+        SceneManager.LoadScene("StartScene");
+    }
+    public void InsufficientBalance()
+    {
+        insufficient.SetActive(false);
     }
 }
